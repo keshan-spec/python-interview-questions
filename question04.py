dba947d5f1178bada9cf4b6888ee1daf219ddee3
keshan-spec <keshanth.programmer@gmail.com>
2020-02-04 11:31:43 +0530

    Question 4: Find first duplicate

---
+++ question04.py
@@ -0,0 +1,41 @@
+"""
+In an integer array made up of values starting from 1 and the length of the array.
+find the first pair of duplicates, example
+    [0,15,2,10] wrong
+    [1,2,3,4] right
+    [1,2,3,2,4,3] -> 2 would be the first duplicate here
+
+    return the index if the duplicate is found, else return -1
+"""
+
+import numpy as np
+import time
+
+x = np.array([1, 2, 3, 4, 2, 3])
+
+
+# checks if the array is valid
+# return : [True ,False ,....]
+def array_valid(array):
+    return [(False if 0 in array or i > len(array) else True) for i in array]
+
+
+# main function that finds the first duplicate
+# return : -1 if there is any error or no duplicates, index and data if the duplicate is found
+def find_first_duplicate(array):
+    now = time.time()
+    if all(array_valid(array)):
+        hash_map = {}
+        for index, data in enumerate(array):
+            if data in hash_map.keys():
+                print(f"Took {time.time() - now}s")
+                return f"Value : {data} has a duplicate in the Index : {index}"
+            else:
+                hash_map[data] = 0
+
+    print(f"Took {time.time() - now}s")
+    return -1
+
+
+print(find_first_duplicate(x))
+
